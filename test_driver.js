a92ad1898e27f41ca25b744130850827874498cc
mizuka-nnomae <mizuka_nnomae@iloud.com>
2021-02-24 01:19:45 +0900

    更新

--- test_driver.js
+++ test_driver.js
@@ -1,6 +1,6 @@
 const MHZ19 = require('mh_z19');
 let   uart  = "/dev/ttyS0";
 
-let mh_z19 = new MHZ19(uart, function(error, co2_level){
+let mh_z19 = new MHZ19(uart, function(error, co2_level, stderr){
   console.log (co2_level);
 });
