fa59bbea7295f2004561c95cf18e50e6e98336de
mizuka-nnomae <mizuka_nnomae@iloud.com>
2021-02-25 00:11:42 +0900

    更新

--- mh_z19.js
+++ mh_z19.js
@@ -1,6 +1,8 @@
-const SerialPort   = require('serialport');
-const sdata        = Buffer.from([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
-let   rdata        = Buffer.alloc(0);
+const SerialPort = require('serialport');
+const ByteLength = require('@serialport/parser-byte-length')
+const sdata      = Buffer.from([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
+let   rdata      = new Uint8Array ();
+let   checksum;
 
 class MHZ19 {
   constructor (uart_path, callback) {
@@ -11,46 +13,45 @@ class MHZ19 {
                                       parity:   'none',
                                       stopBits: 1
     });
+    const parser    = port.pipe (new ByteLength ({length: 9}));
 
     port.on ("open", function () {
       if (require.main === module) {
-        console.log ("MH-Z19 Co2Sensor SerialPort: Open ('" + uart_path + "')");
-        console.log ('MH-Z19 Co2Sensor send data:    ', sdata);
+        console.log ("MH-Z19 SerialPort: Open ('" + uart_path + "')");
+        console.log ('MH-Z19 send data:    ', sdata);
       }
-      port.write (sdata);
-    });
 
-    port.on ("data", function (data) {
-      rdata = Buffer.concat ([rdata, data]);
-      if (rdata.byteLength >= 9) {
-        port.close ();
-      }
-    });
+      port.write (sdata, 'binary', function() {
+        parser.on ("data", function (data) {
+          rdata = data;
+          port.close ();
+        });
 
-    port.on ("error", function (err) {
-      port.close ();
-      callback ("MH-Z19 Co2Sensor SerialPort: error", null, err);
-    });
+        port.on ("error", function (err) {
+          port.close ();
+          callback ("MH-Z19 SerialPort: error", null, err);
+        });
 
-    port.on ("close", function () {
-      if (require.main === module) {
-      console.log ('MH-Z19 Co2Sensor receive data: ', rdata);
-      console.log ("MH-Z19 Co2Sensor SerialPort: Close");
-    }
-      let adata    = new Uint8Array (rdata);
-      let checksum = ((255-(adata[1]+adata[2]+adata[3]+adata[4]+adata[5]+adata[6]+adata[7]))+1);
-      if (adata[8] == checksum) {
-        callback (null, adata[2]*256+adata[3], null);
-      }
-      else {
-        callback ("MH-Z19 Co2Sensor Bad Checksum", adata[2]*256+adata[3], "Checksam value: " + adata[8] + " / Calculated value: " + checksum);
-      }
+        port.on ("close", function () {
+          if (require.main === module) {
+            console.log ('MH-Z19 receive data: ', rdata);
+            console.log ("MH-Z19 SerialPort: Close");
+          }
+          checksum = ((255-(rdata[1]+rdata[2]+rdata[3]+rdata[4]+rdata[5]+rdata[6]+rdata[7]))+1);
+          if (rdata[8] == checksum) {
+            callback (null, rdata[2]*256+rdata[3], null);
+          }
+          else {
+            callback ("MH-Z19 Bad Checksum", rdata[2]*256+rdata[3], "Checksam value: " + rdata[8] + " / Calculated value: " + checksum);
+          }
+        });
+      });
     });
   }
 }
 
 if (require.main === module) {
-  let mh_z19 = new MHZ19 (process.argv[2], function(error, co2_level, stderr){
+  new MHZ19 (process.argv[2], function(error, co2_level, stderr){
     console.log (co2_level);
     console.log (error);
     console.log (stderr);
--- test_driver.js
+++ test_driver.js
@@ -1,6 +1,8 @@
-const MHZ19 = require('mh_z19');
-let   uart  = "/dev/ttyS0";
+const MHZ19     = require('mh_z19');
+let   uart_path = "/dev/ttyS0";
 
-let mh_z19 = new MHZ19(uart, function(error, co2_level, stderr){
+let mh_z19 = new MHZ19(uart_path, function(error, co2_level, stderr){
   console.log (co2_level);
+  console.log (error);
+  console.log (stderr);
 });
